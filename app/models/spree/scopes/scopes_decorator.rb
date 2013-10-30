4a7949ae3fc8a3fb547c9fb06eb552a74190f98c
Pavel Titov <pavel.titov@offsidegaming.com>
2013-10-30 14:22:09 +0400

    Fix a condition where sort order for taxon 0 overridden sort orders set in current taxon

--- app/models/spree/scopes/scopes_decorator.rb
+++ app/models/spree/scopes/scopes_decorator.rb
@@ -4,6 +4,7 @@ Spree::Product.class_eval do
     where(id: Spree::ProductTaxon.select('spree_product_taxons.product_id').
           joins(:taxon).
           where(Spree::Taxon.table_name => { :id => taxon.self_and_descendants.pluck(:id) })
-         )
+         ).
+    where(spree_product_taxons: {taxon_id: taxon.id})
   end
 end
