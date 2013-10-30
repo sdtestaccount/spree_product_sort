7f018e3fd37c1181652192f11dd2550b1e4ad58f
Pavel Titov <pavel.titov@offsidegaming.com>
2013-10-30 14:37:31 +0400

    Correct condition to display products from child taxons too

--- app/models/spree/scopes/scopes_decorator.rb
+++ app/models/spree/scopes/scopes_decorator.rb
@@ -1,10 +1,11 @@
 Spree::Product.class_eval do
   self.add_search_scope :in_taxon do |taxon|
+    taxon_self_and_descendants_ids = taxon.self_and_descendants.pluck(:id)
     select("spree_products.id, spree_products.*").
     where(id: Spree::ProductTaxon.select('spree_product_taxons.product_id').
           joins(:taxon).
-          where(Spree::Taxon.table_name => { :id => taxon.self_and_descendants.pluck(:id) })
+          where(Spree::Taxon.table_name => { :id => taxon_self_and_descendants_ids })
          ).
-    where(spree_product_taxons: {taxon_id: taxon.id})
+    where(spree_product_taxons: { taxon_id: taxon_self_and_descendants_ids })
   end
 end
