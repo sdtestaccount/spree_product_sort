95495130b39888a5fbf91e2bcdde344ed9af50d6
Scott Brown <scott@rsbrown.net>
2014-01-24 13:17:00 -0800

    push product list/sorting down to one page per taxon

--- app/controllers/spree/admin/product_taxons_controller.rb
+++ app/controllers/spree/admin/product_taxons_controller.rb
@@ -1,11 +1,16 @@
 module Spree
 	module Admin
 		class ProductTaxonsController < ResourceController
-		  def positions
+		  def taxons
         # List of taxons, beginnning with a virtual taxons for Home, and then all taxons with pts
         @taxons = []
         @taxons |= Spree::Taxon.joins(:product_taxons).order(:name)
       end
+      
+      def positions
+        @taxon = Spree::Taxon.find(params[:id])
+      end
+
 		end
 	end
 end
