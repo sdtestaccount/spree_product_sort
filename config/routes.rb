cf8e448c4db393a33ea8d58937c21da8f71d298b
Scott Brown <scott@rsbrown.net>
2014-03-17 17:33:57 -0700

    correct mismatch between resource type and id in restful route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Spree::Core::Engine.routes.prepend do
 		# callback for jQuery sort action
     match 'taxons/reorder_products/:id' => 'taxons#reorder_products', :via => :put
     match 'product_taxons/taxons' => 'product_taxons#taxons', :as => :product_sort_taxon
-    match 'product_taxons/:id/positions' => 'product_taxons#positions', :as => :product_sort
+    match 'taxons/:id/positions' => 'product_taxons#positions', :as => :product_sort
     resources :product_taxons do
       collection do
         get :positions
