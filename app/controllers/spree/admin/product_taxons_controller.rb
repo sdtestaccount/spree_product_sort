94e4dde44630889281908da80ff4e794ae853528
Phil Battos <philbattos@gmail.com>
2015-08-20 15:53:58 -0600

    Add debugging

--- app/controllers/spree/admin/product_taxons_controller.rb
+++ app/controllers/spree/admin/product_taxons_controller.rb
@@ -7,12 +7,15 @@ module Spree
       end
 
       def taxons
+        Rails.logger.debug "PARAMS: #{params.inspect}"
+        puts "PARAMS: #{params.inspect}"
         # List of taxons, beginnning with a virtual taxons for Home, and then all taxons with pts
         @taxons = []
         @taxons |= Spree::Taxon.joins(:product_taxons).order(:name)
       end
 
       def positions
+        puts "PARAMS: #{params.inspect}"
         @taxon = Spree::Taxon.find(params[:id])
       end
     end
--- app/controllers/spree/admin/taxons_controller_decorator.rb
+++ app/controllers/spree/admin/taxons_controller_decorator.rb
@@ -1,10 +1,16 @@
 Spree::Admin::TaxonsController.class_eval do
 
   def reorder_products
+    Rails.logger.debug "reorder_products action - TaxonsController"
+    Rails.logger.debug "PARAMS: #{params.inspect}"
     params[:product_taxons].each_with_index do |ptid, idx|
+      Rails.logger.debug "ptid: #{ptid}"
+      Rails.logger.debug "idx: #{idx}"
       pt = Spree::ProductTaxon.find(ptid.to_i)
+      Rails.logger.debug "pt: #{pt}"
       pt.insert_at(idx)
     end
+    Rails.logger.debug "head :created - #{head :created}"
     head :created
   end
 
