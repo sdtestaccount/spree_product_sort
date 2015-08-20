13045f89f6ec729f92bd88d5592032cc88de1a3a
Phil Battos <philbattos@gmail.com>
2015-08-20 14:46:20 -0600

    Add debugging to taxons controller

--- app/controllers/spree/admin/product_taxons_controller.rb
+++ app/controllers/spree/admin/product_taxons_controller.rb
@@ -1,16 +1,20 @@
 module Spree
-	module Admin
-		class ProductTaxonsController < ResourceController
-		  def taxons
+  module Admin
+    class ProductTaxonsController < ResourceController
+      def index
+        Rails.logger.debug "INDEX ACTION - ProductTaxonsController"
+        Rails.logger.debug "PARAMS: #{params.inspect}"
+      end
+
+      def taxons
         # List of taxons, beginnning with a virtual taxons for Home, and then all taxons with pts
         @taxons = []
         @taxons |= Spree::Taxon.joins(:product_taxons).order(:name)
       end
-      
+
       def positions
         @taxon = Spree::Taxon.find(params[:id])
       end
-
-		end
-	end
+    end
+  end
 end
