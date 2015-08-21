576cbb376ddae25ff314687ff75bb952bbc79d45
Phil Battos <philbattos@gmail.com>
2015-08-21 01:00:55 -0600

    Remove extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Spree::Core::Engine.routes.prepend do
-  
+
   namespace :admin do
 		# callback for jQuery sort action
     match 'taxons/reorder_products/:id' => 'taxons#reorder_products', :via => :put
     match 'product_taxons/taxons' => 'product_taxons#taxons', :as => :product_sort_taxon
     match 'taxons/:id/positions' => 'product_taxons#positions', :as => :product_sort
-    resources :product_taxons do
-      collection do
-        get :positions
-      end
-    end
-      
+    # resources :product_taxons do
+    #   collection do
+    #     get :positions
+    #   end
+    # end
+
   end
 end
