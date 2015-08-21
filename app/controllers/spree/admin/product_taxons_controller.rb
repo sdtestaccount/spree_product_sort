b663b450f8ef5ee523f60aa00688fbf58b1b0ccc
Phil Battos <philbattos@gmail.com>
2015-08-21 01:04:29 -0600

    Update debugging

--- app/controllers/spree/admin/product_taxons_controller.rb
+++ app/controllers/spree/admin/product_taxons_controller.rb
@@ -2,13 +2,12 @@ module Spree
   module Admin
     class ProductTaxonsController < ResourceController
       def index
-        Rails.logger.debug "INDEX ACTION - ProductTaxonsController"
-        Rails.logger.debug "PARAMS in index action: #{params.inspect}"
+        puts "INDEX ACTION - ProductTaxonsController"
+        puts "PARAMS in index action: #{params.inspect}"
         render :text => params.inspect
       end
 
       def taxons
-        Rails.logger.debug "PARAMS in taxons action: #{params.inspect}"
         puts "PARAMS in taxons action: #{params.inspect}"
         # List of taxons, beginnning with a virtual taxons for Home, and then all taxons with pts
         @taxons = []
--- app/controllers/spree/admin/taxons_controller_decorator.rb
+++ app/controllers/spree/admin/taxons_controller_decorator.rb
@@ -1,16 +1,16 @@
 Spree::Admin::TaxonsController.class_eval do
 
   def reorder_products
-    Rails.logger.debug "reorder_products action - TaxonsController"
-    Rails.logger.debug "PARAMS in render_products: #{params.inspect}"
+    puts "reorder_products action - TaxonsController"
+    puts "PARAMS in render_products: #{params.inspect}"
     params[:product_taxons].each_with_index do |ptid, idx|
-      Rails.logger.debug "ptid: #{ptid}"
-      Rails.logger.debug "idx: #{idx}"
+      puts "ptid: #{ptid}"
+      puts "idx: #{idx}"
       pt = Spree::ProductTaxon.find(ptid.to_i)
-      Rails.logger.debug "pt: #{pt}"
+      puts "pt: #{pt}"
       pt.insert_at(idx)
     end
-    Rails.logger.debug "head :created - #{head :created}"
+    puts "head :created - #{head :created}"
     head :created
   end
 
