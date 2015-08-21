b970566a74e3737b487abce0858fa1af145efd65
Phil Battos <philbattos@gmail.com>
2015-08-21 00:26:18 -0600

    Remove reordering js post

--- app/controllers/spree/admin/product_taxons_controller.rb
+++ app/controllers/spree/admin/product_taxons_controller.rb
@@ -3,19 +3,20 @@ module Spree
     class ProductTaxonsController < ResourceController
       def index
         Rails.logger.debug "INDEX ACTION - ProductTaxonsController"
-        Rails.logger.debug "PARAMS: #{params.inspect}"
+        Rails.logger.debug "PARAMS in index action: #{params.inspect}"
+        render :text => params.inspect
       end
 
       def taxons
-        Rails.logger.debug "PARAMS: #{params.inspect}"
-        puts "PARAMS: #{params.inspect}"
+        Rails.logger.debug "PARAMS in taxons action: #{params.inspect}"
+        puts "PARAMS in taxons action: #{params.inspect}"
         # List of taxons, beginnning with a virtual taxons for Home, and then all taxons with pts
         @taxons = []
         @taxons |= Spree::Taxon.joins(:product_taxons).order(:name)
       end
 
       def positions
-        puts "PARAMS: #{params.inspect}"
+        puts "PARAMS in positions action: #{params.inspect}"
         @taxon = Spree::Taxon.find(params[:id])
       end
     end
--- app/controllers/spree/admin/taxons_controller_decorator.rb
+++ app/controllers/spree/admin/taxons_controller_decorator.rb
@@ -2,7 +2,7 @@ Spree::Admin::TaxonsController.class_eval do
 
   def reorder_products
     Rails.logger.debug "reorder_products action - TaxonsController"
-    Rails.logger.debug "PARAMS: #{params.inspect}"
+    Rails.logger.debug "PARAMS in render_products: #{params.inspect}"
     params[:product_taxons].each_with_index do |ptid, idx|
       Rails.logger.debug "ptid: #{ptid}"
       Rails.logger.debug "idx: #{idx}"
