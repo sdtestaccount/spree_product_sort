8a53f293d8ae83baa861d5d23d70dba0ab519db5
Sean <seand7565@gmail.com>
2016-09-27 14:44:58 -0500

    Disabling accessors for Rails 4 implementation

--- app/models/spree/product_taxon.rb
+++ app/models/spree/product_taxon.rb
@@ -3,5 +3,6 @@ class Spree::ProductTaxon < ActiveRecord::Base
   belongs_to :taxon
   default_scope :order=>'spree_product_taxons.position'
   acts_as_list :scope=>:taxon
-  attr_accessible :product_id, :taxon_id
+  #Accessors removed for Rails 4
+  #attr_accessible :product_id, :taxon_id
 end
